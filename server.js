7448db4e9567dc4763ef915bd68a904c0ea2f5a1
Nara00 <1806974@ucc.edu.ar>
2022-05-22 20:14:20 -0300

    if production fixed

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(morgan('tiny'));
 app.use('/api', routes);
 
-if(process.eventNames.NODE_ENV === "production") {
+if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req, res) => {
